3403ba96651617fba119a6939ad3a452f12b8677
marsh1100 <margie.b.agu17@gmail.com>
2023-08-06 07:05:30 -0500

    Mostrar vehiculos

--- Program.cs
+++ Program.cs
@@ -39,6 +39,37 @@ internal class Program
             Console.WriteLine("No existen clientes en la base de datos, por lo tanto no puede hacer un registro de un vehículo. ");
         }*/
 
-        mEmpleados.RegistrarEmpleado(DicEmpleado);
+        //mEmpleados.RegistrarEmpleado(DicEmpleado);
+
+        //Registrar Orden de servicio 1.1
+
+        Cliente falsoCliente = new ("Jaime","Lopez",3004785262,"jaime@gmail.com","2023-08-06");
+        DicClientes.Add("111",falsoCliente);
+
+        Vehiculo falsoVehiculo = new ("111","2021","Audi","Azul",1500);
+        DicVehiculos.Add("KTR477",falsoVehiculo);
+        DicVehiculos.Add("UUU000",falsoVehiculo);
+
+
+
+        if(DicClientes.Count != 0)
+        {
+            if(DicVehiculos.Count != 0)
+            {
+                mVehiculos.MostrarVehiculos(DicClientes,DicVehiculos);
+            }else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No vehículos en la base de datos, por lo tanto no puede hacer ninguna orden de servicio");
+                Console.ResetColor();
+            }
+
+        }else{
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("No existen clientes en la base de datos, por lo tanto no puede hacer ninguna orden de servicio");
+            Console.ResetColor();
+        }
+
+        
     }
 }
