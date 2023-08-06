685acc8e19d08b0a40bcd04c7f70372104b37af8
marsh1100 <margie.b.agu17@gmail.com>
2023-08-06 01:39:05 -0500

    feat: :sparkles: Registrar y mostrar clientes

--- Clases/Cliente.cs
+++ Clases/Cliente.cs
@@ -50,5 +50,68 @@ namespace Reparacion_Automotriz.Clases
             set{this.email = value;}
         }
 
+        //Metodos 
+
+        public void RegistrarCliente(Dictionary<string, Cliente> DicClientes)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("********* REGISTRO DE CLIENTE ************");
+            Console.ResetColor();
+
+            Console.WriteLine("Ingrese número de identificación del cliente:");
+            string id = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Ingrese nombre del cliente:");
+            string nombre = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Ingrese apellido del cliente:");
+            string apellido = Convert.ToString(Console.ReadLine());
+            bool invalido = true;
+             do
+            {
+                Console.WriteLine("Ingrese número móvil del cliente:");
+                if(!long.TryParse(Console.ReadLine(), out long telefono) || Convert.ToString(telefono).Length !=10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Digite un número de teléfono válido.");
+                    Console.ResetColor();
+                }else
+                {
+                    invalido = false;
+                }
+                
+            }while(invalido);
+
+            Console.WriteLine("Ingrese email del cliente:");
+            string email = Convert.ToString(Console.ReadLine());
+
+            //validar fecha
+            Console.WriteLine("Ingrese fecha de registro formato (YYYY-MM-DD):");
+            string fecha = Convert.ToString(Console.ReadLine());
+
+            Cliente newCliente = new(nombre,apellido,telefono,email,fecha);
+            DicClientes.Add(id,newCliente);
+
+            Console.WriteLine("Cliente registrado con exito!");
+
+        }
+
+        public void MostrarClientes(Dictionary<string, Cliente> DicClientes)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("******* CLIENTES ******");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("ID\tNombre\tApellido");
+            Console.ResetColor();
+
+            foreach(var cliente in DicClientes)
+            {
+                Console.WriteLine(cliente.Key+"\t"+cliente.Value.Nombre+"\t"+cliente.Value.Apellido);
+            }
+
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿using Reparacion_Automotriz.Clases;
+internal class Program
 {
+    //Diccionario clientes
+    static Dictionary<string, Cliente> DicClientes = new();
+    //Diccionario empleados
+    static Dictionary<string, Empleado> DicEmpleado = new();
+    //Diccionario vehiculos
+    static Dictionary<string, Vehiculo> DicVehiculos = new();
+    //Diccionario  Ordenes de servicios
+    static Dictionary<string, OrdenServicio> DicOrdenesS = new();
+    //Diccionario Diagnósticos de expertos
+    static Dictionary<string, DiagExperto> DicDiagnosticos = new();
+    //Diccionario Ordenes de reparacion
+    static Dictionary<string, OrdenReparacion> DicOrdenesR = new();
+
+
+    //Metodos de las clases
+    static Cliente mClientes = new();
+    static Empleado mEmpleado = new();
+    static Vehiculo mVehiculo = new();
+    static OrdenServicio mOrdenServicio = new();
+    static DiagExperto mDiagExperto = new();
+    static OrdenReparacion mOrdenReparacion = new();
+    
     private static void Main()
     {
-        
+        //Funciona!
+        mClientes.RegistrarCliente(DicClientes);
     }
 }
