2f394086707ea4c6cc52f744de899b1ee64485c5
marsh1100 <margie.b.agu17@gmail.com>
2023-08-07 09:53:54 -0500

    feat: :zap: Arreglo cuando el experto tiene cola de ordenes

--- Clases/DiagExperto.cs
+++ Clases/DiagExperto.cs
@@ -35,76 +35,111 @@ namespace Reparacion_Automotriz.Clases
 
             Console.WriteLine("Digite número de orden para agregar diagnóstico:");
             string idOrden = Convert.ToString(Console.ReadLine());
-
+            foreach(var item in DicOrdenesS.Keys){Console.WriteLine(item);}
             if(DicOrdenesS.ContainsKey(idOrden))
             {
                 if(!DicOrdenesS[idOrden].finalizada){
                     mEmpleados.MostrarEmpleados(DicEmpleados);
-                Console.WriteLine("Digite número de identificación del empleado enacargado de dar el diagnóstico:");
-                string idEmpleado = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Digite número de identificación del empleado enacargado de dar el diagnóstico:");
+                    string idEmpleado = Convert.ToString(Console.ReadLine());
 
-                if(DicDiagnosticos.ContainsKey(idEmpleado) )
-                {
-                    Console.WriteLine("El empleado ya ha registrado un diágnostico a la orden {0} ", idOrden);
-                    Console.WriteLine("Diagnóstico:");
-                    foreach(var item in DicDiagnosticos[idEmpleado].Diagnosticos[idOrden]){
-                        Console.WriteLine("-"+item);
-                    }
-
-                    bool continuar;
-
-                    do{
-                        Console.WriteLine("¿Desea agregar nuevo diágnostico?:\n1.Sí\n2.No");
-
-                        int rta = Int32.Parse(Console.ReadLine());
-                        if(rta ==1)
-                        {   
-                            Console.WriteLine("Ingrese diagnostico:");
-                            DicDiagnosticos[idEmpleado].Diagnosticos[idOrden].Add( Convert.ToString(Console.ReadLine()));
-                            continuar = true;
-                        }else if(rta !=2){
-                            Console.WriteLine("No es un valor válido");
-                            continuar = false;
-                        }else{
-                            continuar = false;
-                        }
-
-                    }while(continuar);
-                    Console.WriteLine("Diagnóstico agregado a la orden {0} de forma correcta", idOrden);
-
-                }else if(DicEmpleados.ContainsKey(idEmpleado)){
-                    bool continuar;
-                    List<string> diagnosticos = new ();
-                    do
+                    if(DicDiagnosticos.ContainsKey(idEmpleado) )
                     {
-                        Console.WriteLine("Ingrese diagnostico del especialista:");
-                        diagnosticos.Add(Convert.ToString(Console.ReadLine()));
 
-                        Console.WriteLine("¿Desea ingresar otro diagnóstico?\n1.Sí\n2.No");
-                        int rta = Int32.Parse(Console.ReadLine());
-                        if(rta ==1)
-                        {
-                            continuar = true;
-                        }else if(rta !=2){
-                            Console.WriteLine("No es un valor válido");
-                            continuar = false;
+                        //Caso en donde ya el experto tiene ordenes en su cola y haya dado un diagnóstico a la orden ingresada
+                        if(DicDiagnosticos[idEmpleado].Diagnosticos.TryGetValue(idOrden, out List<string> diagnosticosO)){
+
+                            Console.WriteLine("El empleado ya ha registrado un diágnostico a la orden {0} ", idOrden);
+                            Console.WriteLine("Diagnóstico:");
+                            foreach(var item in diagnosticosO){
+                                Console.WriteLine("-"+item);
+                            }
+
+                            bool continuar;
+
+                            do{
+                                Console.WriteLine("¿Desea agregar nuevo diágnostico?:\n1.Sí\n2.No");
+
+                                int rta = Int32.Parse(Console.ReadLine());
+                                if(rta ==1)
+                                {   
+                                    Console.WriteLine("Ingrese diagnostico:");
+                                    DicDiagnosticos[idEmpleado].Diagnosticos[idOrden].Add( Convert.ToString(Console.ReadLine()));
+                                    continuar = true;
+                                }else if(rta !=2){
+                                    Console.WriteLine("No es un valor válido");
+                                    continuar = false;
+                                }else{
+                                    continuar = false;
+                                }
+
+                            }while(continuar);
+                            Console.WriteLine("Diagnóstico agregado a la orden {0} de forma correcta", idOrden);
+                        //Caso en donde el experto ya tenga ordenes bajo su cola, pero no haya dado un diagnóstico a la orden ingresada
                         }else{
-                            continuar = false;
+                            bool continuar;
+                            List<string> diagnosticos = new ();
+                            do
+                            {
+                                Console.WriteLine("Ingrese diagnostico del especialista:");
+                                diagnosticos.Add(Convert.ToString(Console.ReadLine()));
+
+                                Console.WriteLine("¿Desea ingresar otro diagnóstico?\n1.Sí\n2.No");
+                                int rta = Int32.Parse(Console.ReadLine());
+                                if(rta ==1)
+                                {
+                                    continuar = true;
+                                }else if(rta !=2){
+                                    Console.WriteLine("No es un valor válido");
+                                    continuar = false;
+                                }else{
+                                    continuar = false;
+                                }
+                            }while(continuar);
+
+                            DicDiagnosticos[idEmpleado].Diagnosticos.Add(idOrden,diagnosticos);
+
+                            Console.WriteLine("Diagnóstico de experto registrado correctamente!.");
+
+
                         }
-                    }while(continuar);
+                        
+                    //Caso en el que el experto no tenga cola de ordenes a su lista.
+                    }else if(DicEmpleados.ContainsKey(idEmpleado)){
 
-                    //Crear diagnostico experto
-                    Dictionary<string, List<string>> dicOrdenE = new();
-                    dicOrdenE.Add(idOrden,diagnosticos);
+                        bool continuar;
+                        List<string> diagnosticos = new ();
+                        do
+                        {
+                            Console.WriteLine("Ingrese diagnostico del especialista:");
+                            diagnosticos.Add(Convert.ToString(Console.ReadLine()));
+
+                            Console.WriteLine("¿Desea ingresar otro diagnóstico?\n1.Sí\n2.No");
+                            int rta = Int32.Parse(Console.ReadLine());
+                            if(rta ==1)
+                            {
+                                continuar = true;
+                            }else if(rta !=2){
+                                Console.WriteLine("No es un valor válido");
+                                continuar = false;
+                            }else{
+                                continuar = false;
+                            }
+                        }while(continuar);
+
+                        //Crear diagnostico experto
+                        Dictionary<string, List<string>> dicOrdenE = new();
+                        dicOrdenE.Add(idOrden,diagnosticos);
+                        
+                        DiagExperto newDiagnostico = new (dicOrdenE);
+                        DicDiagnosticos.Add(idEmpleado, newDiagnostico);
+
+                        Console.WriteLine("Diagnóstico de experto registrado correctamente!.");
                     
-                    DiagExperto newDiagnostico = new (dicOrdenE);
-                    DicDiagnosticos.Add(idEmpleado, newDiagnostico);
+                    }else{
+                        Console.WriteLine("El número de identificación del empleado no se encuentra registrado.");
 
-                    Console.WriteLine("Diagnóstico de experto registrado correctamente!.");
-                }else{
-                    Console.WriteLine("El número de identificación del empleado no se encuentra registrado.");
-
-                }
+                    }
                 }else{
                     Console.WriteLine("La orden ya se encuentra finalizada");
                 }
