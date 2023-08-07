b01d52bc1a55410458574a8dda7e15a0b52271e0
marsh1100 <margie.b.agu17@gmail.com>
2023-08-06 21:06:23 -0500

    Agregar diagnostico experto a una orden

--- Clases/DiagExperto.cs
+++ Clases/DiagExperto.cs
@@ -28,5 +28,54 @@ namespace Reparacion_Automotriz.Clases
             set {this.diagnosticos.AddRange(value);}
         }
         //Métodos
+
+       public void NuevoDiganostico(Dictionary<string, Empleado> DicEmpleados,Dictionary<string, OrdenServicio> DicOrdenesS,Dictionary<string, DiagExperto> DicDiagnosticos)
+       {
+            Empleado mEmpleados = new();
+
+            Console.WriteLine("Digite número de orden para agregar diagnóstico:");
+            string idOrden = Convert.ToString(Console.ReadLine());
+
+            if(DicOrdenesS.ContainsKey(idOrden))
+            {
+                mEmpleados.MostrarEmpleados(DicEmpleados);
+                Console.WriteLine("Digite número de identificación del empleado enacargado de dar el diagnóstico:");
+                string idEmpleado = Convert.ToString(Console.ReadLine());
+
+                if(DicEmpleados.ContainsKey(idEmpleado))
+                {
+                    bool continuar;
+                    List<string> diagnosticos = new ();
+                    do
+                    {
+                        Console.WriteLine("Ingrese diagnostico del especialista:");
+                        diagnosticos.Add(Convert.ToString(Console.ReadLine()));
+
+                        Console.WriteLine("¿Desea ingresar otro diagnóstico?\n1.Sí\n2.No");
+                        int rta = Int32.Parse(Console.ReadLine());
+                        if(rta ==1)
+                        {
+                            continuar = true;
+                        }else if(rta !=2){
+                            Console.WriteLine("No es un valor válido");
+                            continuar = false;
+                        }else{
+                            continuar = false;
+                        }
+                    }while(continuar);
+
+                    DiagExperto newDiagnostico = new (idOrden,diagnosticos);
+                    DicDiagnosticos.Add(idEmpleado, newDiagnostico);
+
+                    Console.WriteLine("Diagnóstico de experto registrado correctamente!.");
+
+                }else{
+                    Console.WriteLine("El número de identificación del empleado no se encuentra registrado.");
+
+                }
+            }else{
+                Console.WriteLine("El n° de orden ingresado no existe en la base de datos. Por favor verificar.");
+            }
+       }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ internal class Program
     //Diccionario clientes
     static Dictionary<string, Cliente> DicClientes = new();
     //Diccionario empleados
-    static Dictionary<string, Empleado> DicEmpleado = new();
+    static Dictionary<string, Empleado> DicEmpleados = new();
     //Diccionario vehiculos
     static Dictionary<string, Vehiculo> DicVehiculos = new();
     //Diccionario  Ordenes de servicios
@@ -39,18 +39,11 @@ internal class Program
             Console.WriteLine("No existen clientes en la base de datos, por lo tanto no puede hacer un registro de un vehículo. ");
         }*/
 
-        //mEmpleados.RegistrarEmpleado(DicEmpleado);
+        //mEmpleados.RegistrarEmpleado(DicEmpleados);
 
 
-        Cliente falsoCliente = new ("Jaime","Lopez",3004785262,"jaime@gmail.com","2023-08-06");
-        DicClientes.Add("111",falsoCliente);
-
-        Vehiculo falsoVehiculo = new ("111","2021","Audi","Azul",1500);
-        DicVehiculos.Add("KTR477",falsoVehiculo);
-        DicVehiculos.Add("UUU000",falsoVehiculo);
-
-        //Registrar Orden de servicio 1.1
-        if(DicClientes.Count != 0)
+        //Registrar Orden de servicio: Datos de la orden 1.1
+        /*if(DicClientes.Count != 0)
         {
             if(DicVehiculos.Count != 0)
             {
@@ -68,7 +61,38 @@ internal class Program
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("No existen clientes en la base de datos, por lo tanto no puede hacer ninguna orden de servicio");
             Console.ResetColor();
-        }
+        }*/
+
+        //Registrar Orden de servicio :diagnostico experto 1.2
+        Cliente falsoCliente = new ("Jaime","Lopez",3004785262,"jaime@gmail.com","2023-08-06");
+        DicClientes.Add("111",falsoCliente);
+
+        Vehiculo falsoVehiculo = new ("111","2021","Audi","Azul",1500);
+        DicVehiculos.Add("KTR477",falsoVehiculo);
+        DicVehiculos.Add("UUU000",falsoVehiculo);
+        
+        List<string> especialidad = new(){"Bailar","Cocinar"};
+        Empleado falsoEmpleado = new("Lucas",3004789625,especialidad);
+        List<string> especialidad2 = new(){"Cocinar"};
+        Empleado falsoEmpleado2 = new("Martin",3004789625,especialidad2);
+        DicEmpleados.Add("999",falsoEmpleado);
+        DicEmpleados.Add("888",falsoEmpleado2);
+
+        OrdenServicio falsaordenServicio= new("111","2023-08-06","UUU000","Motor quemado :c");
+        
+        OrdenServicio falsaordenServicio2= new("111","2023-08-06","KTR477","Motor quemado :c");
+        DicOrdenesS.Add("001", falsaordenServicio);
+        DicOrdenesS.Add("002", falsaordenServicio2);
+        DicOrdenesS.Add("003", falsaordenServicio);
+
+
+
+        mOrdenServicios.MostrarOrdenes(DicOrdenesS,DicDiagnosticos);
+        mDiagExpertos.NuevoDiganostico(DicEmpleados,DicOrdenesS,DicDiagnosticos);
+        mDiagExpertos.NuevoDiganostico(DicEmpleados,DicOrdenesS,DicDiagnosticos);
+
+        mOrdenServicios.MostrarOrdenes(DicOrdenesS,DicDiagnosticos);
+
 
         
     }
