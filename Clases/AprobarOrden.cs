b3929c1de71873d81b7e0b59a269aee661244867
Marsh1100 <margie.b.agu17@gmail.com>
2023-08-08 21:39:32 -0500

    Realizar lista de aprobaciones para factura

--- Clases/AprobarOrden.cs
+++ Clases/AprobarOrden.cs
@@ -5,13 +5,22 @@ namespace Reparacion_Automotriz.Clases
         public string IdOrden { get; set; }
         public string IdEmpleado { get; set; }
 
+        public bool Aprobada {get; set; }
+
         public AprobarOrden(){}
 
-        public AprobarOrden(string idOrden, string idEmpleado)
+        public AprobarOrden(string idOrden, string idEmpleado, bool aprobada = false)
         {
             this.IdOrden = idOrden;
             this.IdEmpleado = idEmpleado;
+            this.Aprobada= aprobada;
         }
 
+        public void NuevaAprobacion(List<AprobarOrden>ListAprobaciones, string idorden, string idEmpleado)
+        {
+            AprobarOrden newAprobarOrden = new (idorden,idEmpleado,true);
+
+            ListAprobaciones.Add(newAprobarOrden);
+        }
     }
 }
